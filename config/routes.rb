c48b777f54c204aeb73a0f3f54094ea11cc2e268
Kevin Gandolfi <gandolfi.kevin@gmail.com>
2022-11-28 17:06:24 +0100

    add new and create routes and actions in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "airplanes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :airplanes, only: %i[index]
+  resources :airplanes, only: %i[index new create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
