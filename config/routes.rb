fdf8aecfe36d645e94b3189f8b0e1047638cfbf9
Kevin Gandolfi <gandolfi.kevin@gmail.com>
2022-11-30 12:08:16 +0100

    started create action in bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "airplanes#index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :airplanes, only: %i[index new show create edit update destroy]
+  resources :airplanes do
+    resources :bookings, only: %i[create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
