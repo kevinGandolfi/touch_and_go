6331e573d0faf85ce7fb63572e3850e7d1d272ee
Kevin Gandolfi <gandolfi.kevin@gmail.com>
2022-12-02 14:55:46 +0100

    implement constraint on dates for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,9 +4,14 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.airplane = @airplane
-    if @booking.save
-      redirect_to airplane_path(@airplane)
+    if valid_interval?(check_intervals(@booking))
+      if @booking.save
+        redirect_to airplane_path(@airplane), notice: "Booking successfully saved"
+      else
+        render "airplanes/show", status: :unprocessable_entity
+      end
     else
+      flash[:alert] = "Dates not available"
       render "airplanes/show", status: :unprocessable_entity
     end
   end
@@ -15,5 +20,23 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_day, :end_day)
+  end
+
+  def check_intervals(booking_airplane)
+    airplane = Airplane.find(params[:airplane_id])
+    bookings = Booking.where(airplane:)
+    check_array = []
+    bookings.each do |booking|
+      if (booking.start_day..booking.end_day).cover?(booking_airplane.start_day) || (booking.start_day..booking.end_day).cover?(booking_airplane.end_day)
+        check_array << false
+      else
+        check_array << true
+      end
+    end
+    check_array
+  end
+
+  def valid_interval?(check_array)
+    !check_array.include?(false)
   end
 end
